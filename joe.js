207aba20c46780d0a142cdd84353da1eb3079e0d
Heungsub Lee <lee@heungsub.net>
2010-07-19 13:54:19 +0900

    Changed field data format. Use `JOE.Field` instead of `Array`.

--- joe.js
+++ joe.js
@@ -1,14 +1,17 @@
+/**
+JOE(http://alankang.tistory.com/137) implement for Classy and jQuery.
+*/
 (function( $, window, undefined ) {
 
-window.JOE = Class.$extend({
-    /** JOE(http://alankang.tistory.com/137) implement for Classy and jQuery.
+var JOE = Class.$extend({
+    /** .. class:: JOE
 
     Define the "name" class.
 
         >>> var Name = JOE.$extend({
         ...     $map: {
-        ...         first: [".first"],
-        ...         last: [".last"]
+        ...         first: JOE.find( ".first" ),
+        ...         last: JOE.find( ".last" )
         ...     },
         ...     $template: '\
         ...       <p class="name"> \
@@ -76,9 +79,9 @@ window.JOE = Class.$extend({
 
         >>> var Person = JOE.$extend({
         ...     $map: {
-        ...         name: [".name", Name],
-        ...         age: [".age", Number],
-        ...         profile: [".profile", "src"]
+        ...         name: JOE.find( ".name" ).as( Name ),
+        ...         age: JOE.find( ".age" ).as( Number ),
+        ...         profile: JOE.find( ".profile" ).attr( "src" )
         ...     },
         ...     older: function() {
         ...         this.age( this.age() + 1 );
@@ -144,8 +147,8 @@ window.JOE = Class.$extend({
 
         >>> var Family = JOE.$extend({
         ...     $map: {
-        ...         head: [".head .person", Person],
-        ...         members: [".members .person", [Person]]
+        ...         head: JOE.find( ".head .person" ).as( Person ),
+        ...         members: JOE.find( ".members .person" ).asArray( Person )
         ...     },
         ...     $template: '\
         ...       <div class="family"> \
@@ -301,14 +304,14 @@ window.JOE = Class.$extend({
 
         >>> var HasProperty = JOE.$extend({
         ...     $map: {
-        ...         text: [null, "text", {
+        ...         text: JOE.text().bind({
         ...             get: function( val ) {
         ...                 return "^" + val + "$";
         ...             },
         ...             set: function( val ) {
         ...                 return val.replace( /^\^|\$$/g, "" );
         ...             }
-        ...         }]
+        ...         })
         ...     }
         ... });
 
@@ -336,121 +339,37 @@ window.JOE = Class.$extend({
         var property = function( prop ) {
             return function( val ) {
                 var isInt = String( parseInt( val ) ) !== "NaN";
-                if ( isInt && this[ prop ].$desc.isArray ) {
+                if ( isInt && this[ prop ].$field.isArray ) {
                     return this[ prop ]()[ val ];
                 } else if ( val === undefined ) {
                     var val = this.__data__[ prop ],
-                        result = this[ prop ].$desc.getter.call( this, val );
+                        getter = this[ prop ].$field.events.get,
+                        result = getter.call( this, val );
                     if ( result !== false && result !== undefined ) {
                         val = result;
                     }
                     return val;
                 } else {
-                    var result = this[ prop ].$desc.setter.call( this, val );
+                    var setter = this[ prop ].$field.events.set,
+                        result = setter.call( this, val );
                     if ( result === false ) {
                         return this;
                     } else if ( result !== undefined ) {
                         val = result;
                     }
-                    if ( this[ prop ].$desc.isArray ) {
+                    if ( this[ prop ].$field.isArray ) {
                         val.add = this.__data__[ prop ].add,
                         val.remove = this.__data__[ prop ].remove;
                     }
                     this.__data__[ prop ] = val;
-                    this[ prop ].$set( this.$attached, val );
+                    this[ prop ].$field.set( this.$attached, val );
                     return this;
                 }
             };
         };
-        var desc, selector, type, attr, isArray;
         for ( var prop in this.$map ) {
-            desc = this.$map[ prop ],
-            type = attr = undefined,
-            getter = setter = function() {},
-            isArray = false;
-            for ( var i = 1; i < desc.length; i++ ) {
-                if ( $.isFunction( desc[ i ] ) ) {
-                    isArray = false;
-                    type = desc[ i ];
-                } else if ( $.isArray( desc[ i ] ) ) {
-                    isArray = true;
-                    type = desc[ i ][ 0 ];
-                } else if ( $.isPlainObject( desc[ i ] ) ) {
-                    getter = desc[ i ].get || getter;
-                    setter = desc[ i ].set || setter;
-                } else if ( typeof desc[ i ] === "string" ) {
-                    attr = desc[ i ];
-                }
-            }
-
-            selector = desc[ 0 ] || null,
-            attr = attr || "text",
-            type = type || String;
-
             this[ prop ] = property( prop );
-
-            this[ prop ].$desc = {
-                selector: selector,
-                attr: attr,
-                type: type,
-                isArray: isArray,
-                getter: getter,
-                setter: setter
-            };
-            this[ prop ].$select = $.proxy(function( elem ) {
-                var fn = this[ 0 ][ this[ 1 ] ];
-                elem = $( elem );
-                if ( fn.$desc.selector ) {
-                    return elem.find( fn.$desc.selector );
-                } else {
-                    return elem;
-                }
-            }, [ this, prop ] );
-            this[ prop ].$get = $.proxy(function( elem ) {
-                var fn = this[ 0 ][ this[ 1 ] ],
-                    attr = fn.$desc.attr,
-                    type = fn.$desc.type,
-                    isArray = fn.$desc.isArray,
-                    cast = function( elem ) {
-                        if ( type.prototype.__proto__ === JOE.prototype ) {
-                            return type.from( elem );
-                        } else {
-                            if ( JOE.isAttr( attr ) ) {
-                                return type( elem.attr( attr ) );
-                            } else {
-                                return type( elem[ attr ]() );
-                            }
-                        }
-                    };
-                elem = fn.$select( elem );
-                if ( isArray ) {
-                    var arr = elem.map(function() {
-                        return cast( this );
-                    }).get();
-                    arr.remove = function( index ) {
-                        var obj = this[ index ];
-                        delete this[ index ];
-                        obj.removeElem();
-                        return obj;
-                    };
-                    arr.add = function( obj ) {
-                        this.push( obj );
-                        return obj;
-                    };
-                    return arr;
-                } else {
-                    return cast( elem );
-                }
-            }, [ this, prop ] );
-            this[ prop ].$set = $.proxy(function( elem, val ) {
-                var fn = this[ 0 ][ this[ 1 ] ];
-                elem = fn.$select( elem );
-                if ( JOE.isAttr( fn.$desc.attr ) ) {
-                    return elem.attr( fn.$desc.attr, val );
-                } else {
-                    return elem[ fn.$desc.attr ]( val );
-                }
-            }, [ this, prop ] );
+            this[ prop ].$field= this.$map[ prop ];
         }
     },
     attach: function( elem, reversed ) {
@@ -458,9 +377,9 @@ window.JOE = Class.$extend({
         this.$attached.data( "joe", this );
         for ( var prop in this.$map ) {
             if ( reversed ) {
-                this.__data__[ prop ] = this[ prop ].$get( elem );
+                this.__data__[ prop ] = this[ prop ].$field.get( elem );
             } else {
-                this[ prop ].$set( this.$attached, this.__data__[ prop ] );
+                this[ prop ].$field.set( this.$attached, this.__data__[ prop ] );
             }
         }
         return this;
@@ -508,5 +427,127 @@ window.JOE = Class.$extend({
         }
     }
 });
+
+/** .. class:: JOE.Field
+
+Mapping field chaining.
+
+    >>> var chain = JOE.find( "section h" ).find( "a" );
+    >>> chain.get( document.body );
+    joe.js
+    >>> chain.set( document.body, "Field test done." );
+    [object Object]
+    >>> chain.get( document.body );
+    Field test done.
+*/
+JOE.Field = function( method, arguments ) {
+    this.chain = [];
+    this.isArray = false;
+    this.type = String;
+    this[ this.defaultAttribute ]();
+    this[ method ].apply( this, arguments );
+    return this;
+};
+
+JOE.Field.prototype = {
+    traversing: [ "find" ],
+    attributes: [ "attr", "html", "text" ],
+    defaultAttribute: "text",
+
+    events: {
+        get: $.noop,
+        set: $.noop
+    },
+
+    select: function( elem ) {
+        var call;
+        elem = $( elem );
+        for ( var i = 0; i < this.chain.length; i++ ) {
+            call = this.chain[ i ];
+            elem = elem[ call.method ].apply( elem, call.arguments );
+        }
+        return elem;
+    },
+    get: function( elem ) {
+        elem = this.select( elem );
+        var call = this.attribute,
+            type = this.type,
+            cast = function( elem, type ) {
+                elem = $( elem );
+                if ( type.prototype.__proto__ === JOE.prototype ) {
+                    return type.from( elem );
+                } else {
+                    var v = elem[ call.method ].apply( elem, call.arguments );
+                    return type( v );
+                }
+            };
+        if ( this.isArray ) {
+            var arr = elem.map(function() {
+                return cast( this, type );
+            }).get();
+            arr.add = function( obj ) {
+                this.push( obj );
+                return obj;
+            };
+            arr.remove = function( index ) {
+                var obj = this[ index ];
+                delete this[ index ];
+                obj.removeElem();
+                return obj;
+            };
+            return arr;
+        } else {
+            return cast( elem, type );
+        }
+    },
+    set: function( elem, val ) {
+        elem = this.select( elem );
+        var call = this.attribute,
+            args = [];
+        for ( var i = 0; i < call.arguments.length; i++ ) {
+            args.push( call.arguments[ i ] );
+        }
+        args.push( val );
+        return elem[ call.method ].apply( elem, args );
+    },
+    bind: function( events ) {
+        this.events = $.extend( {}, this.events, events );
+        return this;
+    },
+    as: function( type ) {
+        this.type = type;
+        return this;
+    },
+    asArray: function( type ) {
+        this.isArray = true;
+        this.type = type;
+        return this;
+    }
+};
+JOE.Field.methods = $.merge(
+    JOE.Field.prototype.traversing,
+    JOE.Field.prototype.attributes
+);
+
+$.each( JOE.Field.methods, function() {
+    var method = String( this );
+    JOE[ method ] = function() {
+        return new this.Field( method, arguments );
+    };
+    JOE.Field.prototype[ method ] = function() {
+        var call = {
+            method: method,
+            arguments: arguments
+        };
+        if ( $.inArray( method, this.attributes ) < 0 ) {
+            this.chain.push(call);
+        } else {
+            this.attribute = call;
+        }
+        return this;
+    };
+});
+
+window.JOE = JOE;
 
 })( jQuery, this );
